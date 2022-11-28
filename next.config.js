73188a803456ffa953d5ecc78ff6e032af5b14f3
leirA123 <100305573+leirA123@users.noreply.github.com>
2022-11-28 02:45:08 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://ogs.google.com' },
+          { key: 'Access-Control-Allow-Origin', value: 'interest-cohort' },
           { key: 'Access-Control-Allow-Methods', value: 'POST' },
           { key: 'Access-Control-Allow-Headers', value: 'X-Playlog-Web', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
         ],
