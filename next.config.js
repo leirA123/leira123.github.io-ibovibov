2dc56f23a8077df59614c12245427e34a0ee630b
leirA123 <100305573+leirA123@users.noreply.github.com>
2022-11-28 02:42:26 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://ogs.google.com' },
+          { key: 'Access-Control-Allow-Methods', value: 'POST' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Playlog-Web', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [];
+  }
+};
